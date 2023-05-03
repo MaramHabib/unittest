389c2d063718feca431ac2b171cb78b069f93ac8
Maram <maramashraf7@gmail.com>
2023-05-03 10:59:44 +0300

    re-uploadfiles

--- src/Factorial.php
+++ src/Factorial.php
@@ -3,31 +3,25 @@
 
 
 class Factorial{
-    public function factorial($num)
-    {
-        if ($n == 0)
-        return 1;
-    return $n * factorial($n - 1);
+    public function Fact($number){
+        if(!is_numeric($number)){
+
+            return null;
+        }
+
+        if(is_float($number)){
+            return null;
+        }
+        if($number<0){
+            return null;
+        }
+        $factorial = 1;
+        for ($i = 1; $i <= $number; $i++){
+        $factorial = $factorial * $i;
+        }
+        return $factorial;
     }
 }
-    // Driver Code
-    $num = 5;
-    echo "Factorial of ", $num, " is ", factorial($num);
-// <?php
-// function factorial_of_a_number($n)
-// {
-//   if($n ==0)
-//     {
-// 	   return 1;
-//     }
-//   else 
-//     {	
-// 	   return $n * fact($n-1);
-//     }
-// 	}
-
-// 
-
 ?>
  
 
--- tests/FactorialTest.php
+++ tests/FactorialTest.php
@@ -9,7 +9,17 @@ class FactorialTest extends TestCase
         $c= new Factorial;
         $result1= $c->Fact(0); 
         $result2= $c->Fact(1);
+        $result3= $c->Fact(5);
+        $result4= $c->Fact(-3);
+        $result5= $c->Fact(1.5);
+        $result6= $c->Fact(false);
+        $result7= $c->Fact('abc');
         $this->assertEquals(1,$result1);
         $this->assertEquals(1,$result2);
+        $this->assertNull($result4);
+        $this->assertNull($result5);
+        $this->assertNull($result6);
+        $this->assertNull($result7);
+        
     }
 }
